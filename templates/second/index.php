f2d91c8f57e8382b7335a44d81cfcdc9fa44ef94
srgg6701 <srgg67@gmail.com>
2015-02-20 18:46:18 +0300

    Установлен раздел "Проектирование и дизайн".

--- templates/second/index.php
+++ templates/second/index.php
@@ -5,13 +5,18 @@
 defined('_JEXEC') or die;
 $tmpl_common = dirname(__FILE__). '/../';
 $common_dir = '_common/';
+
 ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>">
 <head>
     <?php require_once $tmpl_common . $common_dir . 'head.php'; ?>
 </head>
+<?php
+$app = JFactory::getApplication();
+$menu = $app->getMenu()->getActive();
+$pageclass = (is_object($menu))? $menu->params->get('pageclass_sfx'):'';?>
 <body>
-<div id="decoration" class="container">
+<div id="<?php echo $pageclass;?>" class="container">
     <?php require_once $tmpl_common . $common_dir . 'header-nav.php'; ?>
     <jdoc:include type="component" style="none" />
 </div>
