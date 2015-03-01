b08c0402ddbc7d062ddcfa40b8a6228bffc827df
srgg6701 <srgg67@gmail.com>
2015-03-02 01:40:54 +0300

    Вставлен партнёрский фрейм.

--- templates/second/index.php
+++ templates/second/index.php
@@ -23,12 +23,23 @@ endif;?>
 <?php
 if($pageclass!="print-contacts"):
     require_once $tmpl_common . $common_dir . 'parallax.php';
-else:?>
-<script>
-        //window.print();
-</script>
-<?php
 endif;?>
 </div>
+<?php
+if($pageclass=="design"):?>
+    <div id="partners-iframe" style="display: none;">
+        <div id="gallery" class="header-slim-big">Проекты партнеров</div>
+        <iframe style="height: 2800px;" src="http://frame.plans24.ru/s1171/" height="400" width="100%" frameborder="0" scrolling="no"></iframe>
+    </div>
+<script>
+    jQuery(function(){
+        var $=jQuery,
+            iframe=$('#partners-iframe');
+        $(iframe).show();
+        $('#work-examples').after(iframe);
+    });
+</script>
+<?php /* <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script><script type="text/javascript" src="iframe.js"></script> */
+endif;?>
 </body>
 </html>
